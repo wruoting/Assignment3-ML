c31dbaef04f8cc8dbaffb932d12c4bfd6d57a168
ruotingw <wruoting@gmail.com>
2018-10-14 02:59:36 -0400

    Better optimization

--- assignment3.py
+++ assignment3.py
@@ -8,12 +8,10 @@ from sklearn.model_selection import train_test_split
 def model():
     # data
     data = csv_to_dataframe('sine.csv')
-    train_x, test_x, train_y, test_y = train_test_split(data['X'], data['Y'], test_size=0.20, random_state=20)
+    train_x, test_x, train_y, test_y = train_test_split(data['X'], data['Y'], test_size=0.80, random_state=20)
 
     # Parameters
-    number_of_steps = 200
-
-    X = tf.placeholder(tf.float32, [None, 1])
+    number_of_steps = 500
 
     input_test = np.arange(-1.0, 1.0, 0.05, dtype=float)
     feature_columns = [
@@ -23,7 +21,7 @@ def model():
     train_input_fn = tf.estimator.inputs.numpy_input_fn(
         x={"X": np.array(train_x.astype(float))},
         y=np.array(train_y.astype(float)),
-        num_epochs=500,
+        num_epochs=200,
         shuffle=True)
     test_input_fn = tf.estimator.inputs.numpy_input_fn(
         x={"X": np.array(test_x.astype(float))},
@@ -32,13 +30,19 @@ def model():
         shuffle=True)
     predict_input_fn = tf.estimator.inputs.numpy_input_fn(
         x={"X": input_test},
-        shuffle=True)
+        shuffle=False)
 
-    model = tf.estimator.LinearRegressor(feature_columns=feature_columns)
+    model = tf.estimator.DNNRegressor(feature_columns=feature_columns,
+                                      hidden_units=[3, 3],
+                                      optimizer=lambda: tf.train.AdamOptimizer(
+                                        learning_rate=tf.train.exponential_decay(
+                                        learning_rate=0.01,
+                                        global_step=tf.train.get_global_step(),
+                                        decay_steps=10000,
+                                        decay_rate=1))
+                                      )
 
     # train_x_tensor = tf.constant(train_x.astype(float))
-    train_x_dataset = tf.data.Dataset().from_tensors(X)
-    # print(input_train(train_x_dataset))
     model.train(input_fn=train_input_fn, steps=number_of_steps)
     # Evaluate how the model performs on data it has not yet seen.
     eval_result = model.evaluate(input_fn=test_input_fn)
